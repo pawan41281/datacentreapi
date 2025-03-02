dfae0ca9e9cabf3acdb41144b6c60cc63aab4918
Pawan Kumar <pawan41281@gmail.com>
2025-03-02 18:29:37 +0530

    initial commit

---
+++ src/main/java/colt/net/datacentreapi/DatacentreapiApplication.java
@@ -0,0 +1,13 @@
+package colt.net.datacentreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacentreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacentreapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/colt/net/datacentreapi/DatacentreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package colt.net.datacentreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacentreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
