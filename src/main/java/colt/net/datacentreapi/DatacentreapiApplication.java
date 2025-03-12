4165acd7f17aeb0108ba65c85e207373ad067654
pawan <pawan41281@gmail.com>
2025-03-12 19:54:18 +0530

    api_gateway_integration

--- src/main/java/colt/net/datacentreapi/DatacentreapiApplication.java
+++ src/main/java/colt/net/datacentreapi/DatacentreapiApplication.java
@@ -1,20 +1,15 @@
 package colt.net.datacentreapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DatacentreapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatacentreapiApplication.class, args);
 	}
 	
-	@Bean
-	public ModelMapper modelMapper() {
-	    return new ModelMapper();
-	}
-
 }
